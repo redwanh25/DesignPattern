f90ae2e2fad07da1bc5dd5c6b0611aab1b477f77
Redwan Hossain <redwan.hossain@metlife.com.bd>
2023-07-31 13:40:31 +0600

    update

---
+++ Adapter/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Adapter;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //IEmployee emp = new EmployeeAdapter();
+        //string value = emp.GetAllEmployees();
+        //Console.ReadLine();
+    }
+}
